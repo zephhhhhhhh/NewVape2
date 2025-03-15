18eb2dfc26cd636f8862585a31ebbe7e888f6bcc
only love <162806957+onlyabletolove@users.noreply.github.com>
2025-03-14 19:12:16 -0500

    Add files via upload

---
+++ NewMainScript.lua
@@ -0,0 +1,107 @@
+local isfile = isfile or function(file)
+	local suc, res = pcall(function()
+		return readfile(file)
+	end)
+	return suc and res ~= nil and res ~= ''
+end
+
+local delfile = delfile or function(file)
+	writefile(file, '')
+end
+
+local function downloadFile(path, func)
+	if not isfile(path) then
+		local suc, res = pcall(function()
+			return game:HttpGet('https://raw.githubusercontent.com/onlyabletolove/NewVape2/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+		end)
+		if not suc or res == '404: Not Found' then
+			error(res)
+		end
+		if path:find('.lua') then
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+		end
+		writefile(path, res)
+	end
+	return (func or readfile)(path)
+end
+
+local function wipeFolder(path)
+	if not isfolder(path) then return end
+	for _, file in listfiles(path) do
+		if file:find('loader') then continue end
+		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.')) == 1 then
+			delfile(file)
+		end
+	end
+end
+
+for _, folder in {'newvape', 'newvape/games', 'newvape/profiles', 'newvape/assets', 'newvape/libraries', 'newvape/guis'} do
+	if not isfolder(folder) then
+		makefolder(folder)
+	end
+end
+
+if not isfolder("newvape/assets/new") then
+	makefolder("newvape/assets/new")
+end
+
+local _, subbed = pcall(function()
+	return game:HttpGet('https://github.com/onlyabletolove/NewVape2')
+end)
+local commit = subbed:find('currentOid')
+commit = commit and subbed:sub(commit + 13, commit + 52) or nil
+commit = commit and #commit == 40 and commit or 'main'
+if commit == 'main' or (isfile('newvape/profiles/commit.txt') and readfile('newvape/profiles/commit.txt') or '') ~= commit then
+	wipeFolder('newvape')
+	wipeFolder('newvape/games')
+	wipeFolder('newvape/guis')
+	wipeFolder('newvape/libraries')
+end
+writefile('newvape/profiles/commit.txt', commit)
+
+
+local fileNames = {
+    "add.png", "alert.png", "allowedicon.png", "allowedtab.png", "arrowmodule.png",
+    "back.png", "bind.png", "bindbkg.png", "blatanticon.png", "blockedicon.png",
+    "blockedtab.png", "close.png", "closemini.png",
+    "colorpreview.png", "combaticon.png", "customsettings.png", "dots.png", "edit.png",
+    "expandright.png", "expandup.png", "friendstab.png", "guisettings.png", "guislider.png",
+    "guisliderrain.png", "guiv4.png", "guivape.png", "info.png", "inventoryicon.png",
+    "legit.png", "legittab.png", "miniicon.png", "notification.png", "overlaysicon.png",
+    "overlaystab.png", "pin.png", "profilesicon.png", "radaricon.png",
+    "rainbow_1.png", "rainbow_2.png", "rainbow_3.png", "rainbow_4.png",
+    "range.png", "rangearrow.png", "rendericon.png", "rendertab.png", "search.png",
+    "expandicon.png", "targetinfoicon.png", "targetnpc1.png", "targetnpc2.png",
+    "targetplayers1.png", "targetplayers2.png", "targetstab.png", "textguiicon.png",
+    "textv4.png", "textvape.png", "utilityicon.png", "vape.png", "warning.png", "worldicon.png"
+}
+
+local function downloadAsset(fileName)
+    local assetPath = 'newvape/assets/new/' .. fileName
+    if not isfile(assetPath) then
+        local suc, res = pcall(function()
+            return game:HttpGet('https://raw.githubusercontent.com/onlyabletolove/NewVape2/main/assets/new/' .. fileName, true)
+        end)
+        if suc and res ~= '404: Not Found' then
+            writefile(assetPath, res)
+        end
+    end
+end
+
+local function downloadAllAssets()
+    local threads = {}
+    
+    for _, fileName in ipairs(fileNames) do
+        table.insert(threads, coroutine.create(function()
+            downloadAsset(fileName)
+        end))
+    end
+    
+    for _, thread in ipairs(threads) do
+        coroutine.resume(thread)
+    end
+end
+
+downloadAllAssets()
+
+return loadstring(downloadFile('newvape/main.lua'), 'main')()
---
+++ README.md
@@ -0,0 +1,40 @@
+<div align="center">
+  
+  # NewVape2
+
+  <p>A new maintained fork of VapeV4 for Roblox. After the original founder moved on, we've decided to continue the legacy with regular updates and new features.</p>
+</div>
+
+## ⚡ Execute
+
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/onlyabletolove/NewVape2/main/NewMainScript.lua", true))()
+```
+
+## 🌐 Community
+
+<div align="center">
+  
+  [![Discord](https://img.shields.io/badge/Discord-Join%20Now-5865F2?style=for-the-badge&logo=discord&logoColor=white)](https://discord.gg/37PdRkKtG2)
+  [![YouTube](https://img.shields.io/badge/YouTube-Subscribe-FF0000?style=for-the-badge&logo=youtube&logoColor=white)]()
+  [![TikTok](https://img.shields.io/badge/TikTok-Follow-000000?style=for-the-badge&logo=tiktok&logoColor=white)]()
+  
+</div>
+
+## 👨‍💻 Developers
+
+[ILiekCarot](https://github.com/ILiekCarot) - Maintainer
+
+[Isloving](https://github.com/onlyabletolove) - Lead Developer
+
+[Retro](https://github.com/robloxexploiterz) - Exploit Developer
+
+[Zeph](https://slat.cc/zephhh) - Kit Researcher
+
+<br/>
+
+## 🙏 Credits
+
+[7GrandDad](https://github.com/7GrandDadPGN) - Original Founder
+
+[Stav](https://github.com/sstvskids/VapeV4ForRoblox) - Found the base fixed remote/controller paths
