73f088318e870eade452e84353e3778cc316fb53
zeph <skidddinf@gmail.com>
2025-03-16 15:16:56 -0700

    making arsenal better

--- games/286090429.lua
+++ games/286090429.lua
@@ -22,6 +22,9 @@ local color = vape.Libraries.color
 local uipallet = vape.Libraries.uipallet
 local getcustomasset = vape.Libraries.getcustomasset
 
+for _, v in {'AnimationPlayer', 'Blink', 'Freecam', 'Disabler', 'ServerHop', 'SafeWalk', 'Parkour', 'FOV', 'ChatSpammer', 'HitBoxes', 'Killaura', 'MouseTP', 'LongJump', 'Reach', 'AutoClicker'} do
+    vape:Remove(v)
+end
 
 
 run(function()
@@ -81,3 +84,88 @@ run(function()
     })
 end)
 
+run(function()
+    local InfiniteAmmo
+    local Amount
+ -- i was forced into adding the amount, changing it wont really matter. 
+    InfiniteAmmo = vape.Categories.Utility:CreateModule({
+        Name = "InfiniteAmmo",
+        Function = function(callback)
+            if callback then
+                task.spawn(function()
+                    while callback do
+                        local ammoVar = lplr:FindFirstChild("PlayerGui") and lplr.PlayerGui:FindFirstChild("GUI") and 
+                        lplr.PlayerGui.GUI:FindFirstChild("Client") and lplr.PlayerGui.GUI.Client:FindFirstChild("Variables")
+
+                        if ammoVar then
+                            ammoVar.ammocount.Value = Amount.Value   
+                            ammoVar.ammocount2.Value = Amount.Value
+                        end
+                        task.wait()  
+                    end
+                end)
+            end
+        end,
+        Tooltip = "Infinite ammo"
+    })
+    Amount = InfiniteAmmo:CreateSlider({
+        Name = 'Amount',
+        Min = 1,
+        Max = 999,
+        Default = 100,
+        Value = Default  
+    })
+end)
+
+
+run(function()
+    local GunColor = vape.Categories.Render:CreateModule({
+        Name = "GunColor",
+        Function = function(callback)
+            if callback then
+                game:GetService("RunService").RenderStepped:Connect(function()
+                    if game.Workspace.Camera:FindFirstChild('Arms') then
+                        for i, v in pairs(game.Workspace.Camera.Arms:GetDescendants()) do
+                            if v.ClassName == 'MeshPart' then
+                                v.Color = cColor
+                                if CustomMaterial then
+                                    v.Material = GunMaterial
+                                end
+                            end
+                        end
+                    end 
+                end)
+            end
+        end,
+        Tooltip = "Changes the gun colors"
+    })
+    
+    Material = GunColor:CreateDropdown({
+        Name = 'Material',
+        List = {'ForceField', 'Neon', 'SmoothPlastic', 'Plastic'},
+        Function = function(choice)
+            if choice == "Plastic" then
+                GunMaterial = Enum.Material.Plastic
+            elseif choice == "SmoothPlastic" then
+                GunMaterial = Enum.Material.SmoothPlastic
+            elseif choice == "Neon" then
+                GunMaterial = Enum.Material.Neon
+            elseif choice == "ForceField" then
+                GunMaterial = Enum.Material.ForceField
+            end
+        end
+    })
+
+    Color = GunColor:CreateColorSlider({
+        Name = 'Color',
+        Function = function(hue, sat, val)
+            cColor = Color3.fromHSV(hue, sat, val)
+        end
+    })
+    CustomMaterialToggle = GunColor:CreateToggle({
+        Name = 'Custom Material',
+        Function = function(enabled)
+            CustomMaterial = enabled
+        end
+    })
+end)
