8dc7e8217976ba7279b0ec5888cf5924fea4fdc5
zeph <skidddinf@gmail.com>
2025-03-17 10:32:50 -0700

    Bro i upload the wrong rivals file :skull:

--- games/17625359962.lua
+++ games/17625359962.lua
@@ -1,159 +1,418 @@
-print("Rivals")
-print("game :" .. game.PlaceId)
-warn("NewAstroid loaded!")
+-- rivals 
 
 
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/lolpoppyus/Roblox-Lua/master/Pop%20UI%20Lib", true))()
-local Players = game:GetService("Players")
+local run = function(func) 
+	func() 
+end
+local cloneref = cloneref or function(obj) 
+	return obj 
+end
 
-local Combat = library:Tab("Combat")
+local playersService = cloneref(game:GetService('Players'))
+local inputService = cloneref(game:GetService('UserInputService'))
+local replicatedStorage = cloneref(game:GetService('ReplicatedStorage'))
+local runService = cloneref(game:GetService('RunService'))
+local tweenService = cloneref(game:GetService('TweenService'))
 
+local gameCamera = workspace.CurrentCamera
+local lplr = playersService.LocalPlayer
 
-Combat:Label("SilentAim")
+local vape = shared.vape
+local entitylib = vape.Libraries.entity
+local targetinfo = vape.Libraries.targetinfo
+local prediction = vape.Libraries.prediction
+local color = vape.Libraries.color
+local uipallet = vape.Libraries.uipallet
+local getcustomasset = vape.Libraries.getcustomasset
 
-Combat:Toggle("SilentAim", function(arg)
-   print(arg)
-end)
+for _, v in {'AnimationPlayer', 'Blink', 'Freecam', 'Disabler', 'FOV', 'ServerHop', 'SafeWalk', 'Parkour', 'HitBoxes', 'Killaura', 'Reach', 'AutoClicker'} do
+    vape:Remove(v)
+end
 
-
-Combat:Toggle("FOV", function(arg)
-    print(arg)
- end)
-
-
- Combat:Slider("HeadshotChance", 0, 100, function(arg)
-    print(arg)
- end)
-
- Combat:Slider("FOV Size", 0, 360, function(arg)
-    print(arg)
- end)
-
- Combat:Colorpicker("FOV Color", function(arg)
-    print(arg)
-end)
  
+local mouseClicked
+run(function()
+	local SilentAim
+	local Target
+	local Mode
+	local Method
+	local MethodRay
+	local IgnoredScripts
+	local Range
+	local HitChance
+	local HeadshotChance
+	local AutoFire
+	local AutoFireShootDelay
+	local AutoFireMode
+	local AutoFirePosition
+	local Wallbang
+	local CircleColor
+	local CircleTransparency
+	local CircleFilled
+	local CircleObject
+	local Projectile
+	local ProjectileSpeed
+	local ProjectileGravity
+	local RaycastWhitelist = RaycastParams.new()
+	RaycastWhitelist.FilterType = Enum.RaycastFilterType.Include
+	local ProjectileRaycast = RaycastParams.new()
+	ProjectileRaycast.RespectCanCollide = true
+	local fireoffset, rand, delayCheck = CFrame.identity, Random.new(), tick()
+	local oldnamecall, oldray
+
+	local function getTarget(origin, obj)
+		if rand.NextNumber(rand, 0, 100) > (AutoFire.Enabled and 100 or HitChance.Value) then return end
+		local targetPart = (rand.NextNumber(rand, 0, 100) < (AutoFire.Enabled and 100 or HeadshotChance.Value)) and 'Head' or 'RootPart'
+		local ent = entitylib['Entity'..Mode.Value]({
+			Range = Range.Value,
+			Wallcheck = Target.Walls.Enabled and (obj or true) or nil,
+			Part = targetPart,
+			Origin = origin,
+			Players = Target.Players.Enabled,
+			NPCs = Target.NPCs.Enabled
+		})
+
+		if ent then
+			targetinfo.Targets[ent] = tick() + 1
+			if Projectile.Enabled then
+				ProjectileRaycast.FilterDescendantsInstances = {gameCamera, ent.Character}
+				ProjectileRaycast.CollisionGroup = ent[targetPart].CollisionGroup
+			end
+		end
+
+		return ent, ent and ent[targetPart], origin
+	end
+
+	local Hooks = {
+		Raycast = function(args)
+			if MethodRay.Value ~= 'All' and args[3] and args[3].FilterType ~= Enum.RaycastFilterType[MethodRay.Value] then return end
+			local ent, targetPart, origin = getTarget(args[1])
+			if not ent then return end
+			args[2] = CFrame.lookAt(origin, targetPart.Position).LookVector * args[2].Magnitude
+			if Wallbang.Enabled then
+				RaycastWhitelist.FilterDescendantsInstances = {targetPart}
+				args[3] = RaycastWhitelist
+			end
+		end
+	}
+	Hooks.FindPartOnRayWithWhitelist = Hooks.FindPartOnRayWithIgnoreList
+	Hooks.FindPartOnRay = Hooks.FindPartOnRayWithIgnoreList
+	Hooks.ViewportPointToRay = Hooks.ScreenPointToRay
+
+	SilentAim = vape.Categories.Combat:CreateModule({
+		Name = 'SilentAim',
+		Function = function(callback)
+			if CircleObject then
+				CircleObject.Visible = callback and Mode.Value == 'Mouse'
+			end
+			if callback then
+				if Method.Value == 'Ray' then
+					oldray = hookfunction(Ray.new, function(origin, direction)
+						if checkcaller() then
+							return oldray(origin, direction)
+						end
+						local calling = getcallingscript()
+
+						if calling then
+							local list = #IgnoredScripts.ListEnabled > 0 and IgnoredScripts.ListEnabled or {'ControlScript', 'ControlModule'}
+							if table.find(list, tostring(calling)) then
+								return oldray(origin, direction)
+							end
+						end
+
+						local args = {origin, direction}
+						Hooks.Ray(args)
+						return oldray(unpack(args))
+					end)
+				else
+					oldnamecall = hookmetamethod(game, '__namecall', function(...)
+						if getnamecallmethod() ~= Method.Value then
+							return oldnamecall(...)
+						end
+						if checkcaller() then
+							return oldnamecall(...)
+						end
+
+						local calling = getcallingscript()
+						if calling then
+							local list = #IgnoredScripts.ListEnabled > 0 and IgnoredScripts.ListEnabled or {'ControlScript', 'ControlModule'}
+							if table.find(list, tostring(calling)) then
+								return oldnamecall(...)
+							end
+						end
+
+						local self, args = ..., {select(2, ...)}
+						local res = Hooks[Method.Value](args)
+						if res then
+							return unpack(res)
+						end
+						return oldnamecall(self, unpack(args))
+					end)
+				end
+
+				repeat
+					if CircleObject then
+						CircleObject.Position = inputService:GetMouseLocation()
+					end
+					if AutoFire.Enabled then
+						local origin = AutoFireMode.Value == 'Camera' and gameCamera.CFrame or entitylib.isAlive and entitylib.character.RootPart.CFrame or CFrame.identity
+						local ent = entitylib['Entity'..Mode.Value]({
+							Range = Range.Value,
+							Wallcheck = Target.Walls.Enabled or nil,
+							Part = 'Head',
+							Origin = (origin * fireoffset).Position,
+							Players = Target.Players.Enabled,
+							NPCs = Target.NPCs.Enabled
+						})
+
+						if mouse1click and (isrbxactive or iswindowactive)() then
+							if ent and canClick() then
+								if delayCheck < tick() then
+									if mouseClicked then
+										mouse1release()
+										delayCheck = tick() + AutoFireShootDelay.Value
+									else
+										mouse1press()
+									end
+									mouseClicked = not mouseClicked
+								end
+							else
+								if mouseClicked then
+									mouse1release()
+								end
+								mouseClicked = false
+							end
+						end
+					end
+					task.wait()
+				until not SilentAim.Enabled
+			else
+				if oldnamecall then
+					hookmetamethod(game, '__namecall', oldnamecall)
+				end
+				if oldray then
+					hookfunction(Ray.new, oldray)
+				end
+				oldnamecall, oldray = nil, nil
+			end
+		end,
+		ExtraText = function()
+			return Method.Value:gsub('FindPartOnRay', '')
+		end,
+		Tooltip = 'Silently adjusts your aim towards the enemy'
+	})
+	Target = SilentAim:CreateTargets({Players = true})
+	Mode = SilentAim:CreateDropdown({
+		Name = 'Mode',
+		List = {'Mouse', 'Position'},
+		Function = function(val)
+			if CircleObject then
+				CircleObject.Visible = SilentAim.Enabled and val == 'Mouse'
+			end
+		end,
+		Tooltip = 'Mouse - Checks for entities near the mouses position\nPosition - Checks for entities near the local character'
+	})
+	Method = SilentAim:CreateDropdown({
+		Name = 'Method',
+		List = {'Raycast'},
+		Function = function(val)
+			if SilentAim.Enabled then
+				SilentAim:Toggle()
+				SilentAim:Toggle()
+			end
+			MethodRay.Object.Visible = val == 'Raycast'
+		end,
+		Tooltip = 'FindPartOnRay* - Deprecated methods of raycasting used in old games\nRaycast - The modern raycast method\nPointToRay - Method to generate a ray from screen coords\nRay - Hooking Ray.new'
+	})
+	MethodRay = SilentAim:CreateDropdown({
+		Name = 'Raycast Type',
+		List = {'All', 'Exclude', 'Include'},
+		Darker = true,
+		Visible = false
+	})
+	IgnoredScripts = SilentAim:CreateTextList({Name = 'Ignored Scripts'})
+	Range = SilentAim:CreateSlider({
+		Name = 'Range',
+		Min = 1,
+		Max = 1000,
+		Default = 150,
+		Function = function(val)
+			if CircleObject then
+				CircleObject.Radius = val
+			end
+		end,
+		Suffix = function(val)
+			return val == 1 and 'stud' or 'studs'
+		end
+	})
+	HitChance = SilentAim:CreateSlider({
+		Name = 'Hit Chance',
+		Min = 0,
+		Max = 100,
+		Default = 85,
+		Suffix = '%'
+	})
+	HeadshotChance = SilentAim:CreateSlider({
+		Name = 'Headshot Chance',
+		Min = 0,
+		Max = 100,
+		Default = 65,
+		Suffix = '%'
+	})
+	AutoFire = SilentAim:CreateToggle({
+		Name = 'AutoFire',
+		Function = function(callback)
+			AutoFireShootDelay.Object.Visible = callback
+			AutoFireMode.Object.Visible = callback
+			AutoFirePosition.Object.Visible = callback
+		end
+	})
+	AutoFireShootDelay = SilentAim:CreateSlider({
+		Name = 'Next Shot Delay',
+		Min = 0,
+		Max = 1,
+		Decimal = 100,
+		Visible = false,
+		Darker = true,
+		Suffix = function(val)
+			return val == 1 and 'second' or 'seconds'
+		end
+	})
+	AutoFireMode = SilentAim:CreateDropdown({
+		Name = 'Origin',
+		List = {'RootPart', 'Camera'},
+		Visible = false,
+		Darker = true,
+		Tooltip = 'Determines the position to check for before shooting'
+	})
+	AutoFirePosition = SilentAim:CreateTextBox({
+		Name = 'Offset',
+		Function = function()
+			local suc, res = pcall(function()
+				return CFrame.new(unpack(AutoFirePosition.Value:split(',')))
+			end)
+			if suc then fireoffset = res end
+		end,
+		Default = '0, 0, 0',
+		Visible = false,
+		Darker = true
+	})
+	Wallbang = SilentAim:CreateToggle({Name = 'Wallbang'})
+	SilentAim:CreateToggle({
+		Name = 'Range Circle',
+		Function = function(callback)
+			if callback then
+				CircleObject = Drawing.new('Circle')
+				CircleObject.Filled = CircleFilled.Enabled
+				CircleObject.Color = Color3.fromHSV(CircleColor.Hue, CircleColor.Sat, CircleColor.Value)
+				CircleObject.Position = vape.gui.AbsoluteSize / 2
+				CircleObject.Radius = Range.Value
+				CircleObject.NumSides = 100
+				CircleObject.Transparency = 1 - CircleTransparency.Value
+				CircleObject.Visible = SilentAim.Enabled and Mode.Value == 'Mouse'
+			else
+				pcall(function()
+					CircleObject.Visible = false
+					CircleObject:Remove()
+				end)
+			end
+			CircleColor.Object.Visible = callback
+			CircleTransparency.Object.Visible = callback
+			CircleFilled.Object.Visible = callback
+		end
+	})
+	CircleColor = SilentAim:CreateColorSlider({
+		Name = 'Circle Color',
+		Function = function(hue, sat, val)
+			if CircleObject then
+				CircleObject.Color = Color3.fromHSV(hue, sat, val)
+			end
+		end,
+		Darker = true,
+		Visible = false
+	})
+	CircleTransparency = SilentAim:CreateSlider({
+		Name = 'Transparency',
+		Min = 0,
+		Max = 1,
+		Decimal = 10,
+		Default = 0.5,
+		Function = function(val)
+			if CircleObject then
+				CircleObject.Transparency = 1 - val
+			end
+		end,
+		Darker = true,
+		Visible = false
+	})
+	CircleFilled = SilentAim:CreateToggle({
+		Name = 'Circle Filled',
+		Function = function(callback)
+			if CircleObject then
+				CircleObject.Filled = callback
+			end
+		end,
+		Darker = true,
+		Visible = false
+	})
+	Projectile = SilentAim:CreateToggle({
+		Name = 'Projectile',
+		Function = function(callback)
+			ProjectileSpeed.Object.Visible = callback
+			ProjectileGravity.Object.Visible = callback
+		end
+	})
+	ProjectileSpeed = SilentAim:CreateSlider({
+		Name = 'Speed',
+		Min = 1,
+		Max = 1000,
+		Default = 1000,
+		Darker = true,
+		Visible = false,
+		Suffix = function(val)
+			return val == 1 and 'stud' or 'studs'
+		end
+	})
+	ProjectileGravity = SilentAim:CreateSlider({
+		Name = 'Gravity',
+		Min = 0,
+		Max = 192.6,
+		Default = 192.6,
+		Darker = true,
+		Visible = false
+	})
+end)
+
+run(function()
+    local AFlashbang
+    AFlashbang = vape.Categories.Utility:CreateModule({
+        Name = "Anti-Flashbang",
+        Function = function(callback)
+            if callback then
+             game:GetService("Players").LocalPlayer.PlayerScripts.Assets.Misc.FlashbangEffect:Destroy()
+             game:GetService("Players").LocalPlayer.PlayerScripts.Modules.ViewModels.Flashbang:Destroy()
+             game:GetService("Players").LocalPlayer.PlayerScripts.UserInterface.FlashbangGui:Destroy()
+            end
+        end,
+        Tooltip = "Disables all flashbanging"
+    })
+end)
+
+run(function()
+    local ThirdPerson
+    ThirdPerson = vape.Categories.Utility:CreateModule({
+        Name = "ThirdPerson",
+        Function = function(callback)
+            if callback then
+                local CameraController = require(lplr.PlayerScripts.Controllers.CameraController)
+                CameraController:SetPOV(false, 0, false)
+            else 
+                vape:CreateNotification('Vape', 'Press B, to return to first person', 5, 'alert')
+
+            end
+        end,
+        Tooltip = "Turns you camera to third person. Press B to disable it"
+    })
+end)
+
  
-
-
-local Player = library:Tab("Player")
-
-
-local Visuals = library:Tab("Visuals")
-
-Visuals:Label("ESP")
-
-local Players = game:GetService("Players")
-
-local InsideTransparency = 0.5
-local OutlineTransparency = 0.5
-
-Visuals:Toggle("ESP", function(state)
-    if state then
-        for _, player in ipairs(Players:GetPlayers()) do
-            if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-                local char = player.Character
-                local highlight = Instance.new("Highlight")
-                highlight.Parent = char
-                highlight.Name = "ESPHighlight"
-                highlight.Adornee = char
-                highlight.FillColor = InsideColor
-                highlight.OutlineColor = OutlineColor
-                highlight.FillTransparency = InsideTransparency
-                highlight.OutlineTransparency = OutlineTransparency
-                highlight.Enabled = true
-            end
-        end
-    else
-        for _, player in ipairs(Players:GetPlayers()) do
-            if player.Character then
-                local char = player.Character
-                local highlight = char:FindFirstChild("ESPHighlight")
-                if highlight then
-                    highlight:Destroy()
-                end
-            end
-        end
-    end
-end)
-
-Visuals:Colorpicker("Inside Color", function(arg)
-    InsideColor = arg
-    for _, player in ipairs(Players:GetPlayers()) do
-        if player.Character then
-            local highlight = player.Character:FindFirstChild("ESPHighlight")
-            if highlight then
-                highlight.FillColor = InsideColor
-            end
-        end
-    end
-end)
-
-Visuals:Colorpicker("Outline Color", function(arg)
-    OutlineColor = arg
-    for _, player in ipairs(Players:GetPlayers()) do
-        if player.Character then
-            local highlight = player.Character:FindFirstChild("ESPHighlight")
-            if highlight then
-                highlight.OutlineColor = OutlineColor
-            end
-        end
-    end
-end)
-
-Visuals:Slider("Inside Transparency", 1, 10, function(arg)
-    InsideTransparency = arg / 10
-    for _, player in ipairs(Players:GetPlayers()) do
-        if player.Character then
-            local highlight = player.Character:FindFirstChild("ESPHighlight")
-            if highlight then
-                highlight.FillTransparency = InsideTransparency
-            end
-        end
-    end
-end)
-
-Visuals:Slider("Outline Transparency", 1, 10, function(arg)
-    OutlineTransparency = arg / 10
-    for _, player in ipairs(Players:GetPlayers()) do
-        if player.Character then
-            local highlight = player.Character:FindFirstChild("ESPHighlight")
-            if highlight then
-                highlight.OutlineTransparency = OutlineTransparency
-            end
-        end
-    end
-end)
-
-
-
-
-
-local UI = library:Tab("Misc")
-
-
-UI:Textstring2("Created by - Zeph")
-
-UI:Button("Rejoin", function()
-    local tspmo = game:GetService("TeleportService")
-    local plr = game:GetService("Players").LocalPlayer
-    tspmo:Teleport(game.PlaceId, plr)
- end)
-
-UI:Keybind("UI", function(enabled)
-    game:GetService("CoreGui").OfficialUILib.Enabled = not game:GetService("CoreGui").OfficialUILib.Enabled;
- end,Enum.KeyCode.P)
- 
-
-while wait(1) do
-   local players = game.Players:GetChildren()
-   local array = {}
-
-   for i,v in pairs(players) do
-       table.insert(array,v.Name)
-   end
-
-   update(array)
-   update2(array)
-end;
-
