02bce2e4b848cc4ae760e098ee6ec781d4e8f8fa
zeph <skidddinf@gmail.com>
2025-03-29 20:25:21 -0700

    proj aura patch

--- games/8768229691.lua
+++ games/8768229691.lua
@@ -1,3 +1,4 @@
+--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.
 local run = function(func)
 	func()
 end
@@ -305,8 +306,6 @@ run(function()
 	local roactCheck = replicatedStorage['rbxts_include']['node_modules']['@rbxts']:FindFirstChild('roact')
 	skywars = setmetatable({
 		CameraUtil = require(lplr.PlayerScripts.TS.util['camera-util']).CameraUtil,
-		FireOrigin = debug.getupvalue(ControllerTable.ProjectileController.chargeBow, 10).ORIGIN_OFFSET,
-		Gravity = debug.getupvalue(ControllerTable.ProjectileController.chargeBow, 12).WORLD_ACCELERATION.Y,
 		ItemMeta = debug.getupvalue(ControllerTable.HotbarController.getSword, 1),
 		Remotes = debug.getupvalue(ControllerTable.MeleeController.strikeDesktop, 6),
 		Roact = require(roactCheck and roactCheck.src or replicatedStorage['rbxts_include']['node_modules']['@rbxts'].ReactLua['node_modules']['@jsdotlua']['roact-compat']),
@@ -1158,85 +1157,7 @@ run(function()
 		Max = 1000,
 		Default = 1000
 	})
-end)
-	
-run(function()
-	local ProjectileAura
-	local Targets
-	local Range
-	local List
-	local rayCheck = RaycastParams.new()
-	rayCheck.FilterType = Enum.RaycastFilterType.Exclude
-	local FireDelays = {}
-	
-	local function getProjectiles()
-		local items = {}
-		for slot, item in store.inventory do
-			item = skywars.ItemMeta[item.Type]
-			if item.Ranged and table.find(List.ListEnabled, item.Ranged.ProjectileType) and getItem(item.Ranged.ProjectileType) then
-				table.insert(items, item)
-			end
-		end
-		return items
-	end
-	
-	ProjectileAura = vape.Categories.Blatant:CreateModule({
-		Name = 'ProjectileAura',
-		Function = function(callback)
-			if callback then
-				repeat
-					local ent = entitylib.EntityPosition({
-						Part = 'RootPart',
-						Range = Range.Value,
-						Players = Targets.Players.Enabled,
-						NPCs = Targets.NPCs.Enabled,
-						Wallcheck = Targets.Walls.Enabled
-					})
-	
-					if ent then
-						local offsetpos = entitylib.character.RootPart.CFrame * skywars.FireOrigin
-						for _, item in getProjectiles() do
-							if (FireDelays[item] or 0) < tick() then
-								rayCheck.FilterDescendantsInstances = {ent.Character, gameCamera}
-								rayCheck.CollisionGroup = ent.RootPart.CollisionGroup
-								local calc = prediction.SolveTrajectory(offsetpos.Position, 200, math.abs(skywars.Gravity), ent.RootPart.Position, ent.RootPart.Velocity, workspace.Gravity, ent.HipHeight, nil, rayCheck)
-								
-								if calc then
-									targetinfo.Targets[ent] = tick() + 1
-									FireDelays[item] = tick() + 0.5
-									skywars.Remotes[remotes.updateActiveItem]:fire(item.Name)
-									skywars.Remotes[remotes.chargeBow]:fire(CFrame.new(offsetpos.Position, calc).LookVector, 1)
-									skywars.Remotes[remotes.updateActiveItem](store.hand.Name) 
-									break
-								end
-							end
-						end
-					end
-					task.wait(0.1)
-				until not ProjectileAura.Enabled
-			end
-		end,
-		Tooltip = 'Shoots people around you'
-	})
-	Targets = ProjectileAura:CreateTargets({
-		Players = true, 
-		Walls = true
-	})
-	List = ProjectileAura:CreateTextList({
-		Name = 'Projectiles',
-		Default = {'Arrow', 'Snowball', 'Capybara'}
-	})
-	Range = ProjectileAura:CreateSlider({
-		Name = 'Range',
-		Min = 1,
-		Max = 50,
-		Default = 50,
-		Suffix = function(val) 
-			return val == 1 and 'stud' or 'studs' 
-		end
-	})
-end)
-	
+end) 
 run(function()
 	local Scaffold
 	local Expand
